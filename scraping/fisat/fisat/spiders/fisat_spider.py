9c03b4e59eed1b97dc3cb9d08471315dc1229951
akshaykrishna155@gmail.com <akshaykrishna155@gmail.com>
2019-10-30 23:26:29 +0530

    spider modified

--- scraping/fisat/fisat/spiders/fisat_spider.py
+++ scraping/fisat/fisat/spiders/fisat_spider.py
@@ -5,11 +5,17 @@ from ..items import FisatItem
 
 class FisatSpySpider(scrapy.Spider):
     name = 'fisat'
-    allowed_domains = ['www.fisat.ac.in']
-    start_urls = ['http://www.fisat.ac.in/']
+    allowed_domains = ['www.amazon.in']
+    start_urls = ['https://www.amazon.in/']
+
+    #def __init__(self):
+      #  for line in open('./domains.txt', 'r').readlines():
+       #     self.allowed_domains.append(line)
+        #    self.start_urls.append('http://%s' % line)
 
     def parse(self, response):
-        #data = response.css('a::text,p::text').extract()
+        data1 = response.css('a::text,p::text').extract()
+        yield {'home':data1}
         for l in response.css("a::attr('href')"):
             new_url = response.urljoin(l.extract())
             yield scrapy.Request(new_url,callback=self.parse_url)
